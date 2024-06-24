b4e0605237fa7a2a8414871da6d728682045d8ca
Vinita Santosh Sonwane <vinitasonwane24@gmail.com>
2024-06-24 15:09:07 +0530

    Create Summed Matrix.java

---
+++ Summed Matrix.java
@@ -0,0 +1,10 @@
+class Solution {
+    static long sumMatrix(long n, long q) {
+        // Calculate the range of valid i values
+        long start = Math.max(1, q-n);
+        long end = Math.min(n , q-1);
+        
+        long count = Math.max(0, end - start +1);
+        return count ;
+    }
+}
