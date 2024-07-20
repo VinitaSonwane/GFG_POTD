6544b1d640ca3e40530fe93d5b9698c36363ee0f
Vinita Santosh Sonwane <vinitasonwane24@gmail.com>
2024-07-20 21:52:46 +0530

    Create Remove Half Nodes.java

---
+++ Remove Half Nodes.java
@@ -0,0 +1,13 @@
+class Solution {
+    
+    public Node RemoveHalfNodes(Node root) {
+        if(root==null)return null;
+        root.left = RemoveHalfNodes(root.left);
+        root.right = RemoveHalfNodes(root.right);
+        
+        if(root.left == null && root.right != null)return root.right;
+        if(root.left != null && root.right==null)return root.left;
+        return root ;
+        
+    }
+}
